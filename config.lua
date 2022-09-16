e777ec3546bfebb8b0455e3808d62fb21457d23c
Squad 5 <nago.thegloriouswalrus@gmail.com>
2022-09-15 20:44:38 -0500

    Disable driving school, move CH to BC

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target in
 
 Config.Cityhalls = {
     { -- Cityhall 1
-        coords = vec3(-265.0, -963.6, 31.2),
+        coords = vector3(-145.78, 6304.15, 31.56),
         showBlip = true,
         blipData = {
             sprite = 487,
@@ -35,7 +35,7 @@ Config.Cityhalls = {
 Config.DrivingSchools = {
     { -- Driving School 1
         coords = vec3(240.3, -1379.89, 33.74),
-        showBlip = true,
+        showBlip = false,
         blipData = {
             sprite = 225,
             display = 4,
@@ -55,7 +55,7 @@ Config.Peds = {
     -- Cityhall Ped
     {
         model = 'a_m_m_hasjew_01',
-        coords = vec4(-262.79, -964.18, 30.22, 181.71),
+        coords = vector4(-146.21, 6303.64, 30.55, 317.95),
         scenario = 'WORLD_HUMAN_STAND_MOBILE',
         cityhall = true,
         zoneOptions = { -- Used for when UseTarget is false
@@ -69,7 +69,7 @@ Config.Peds = {
         model = 'a_m_m_eastsa_02',
         coords = vec4(240.91, -1379.2, 32.74, 138.96),
         scenario = 'WORLD_HUMAN_STAND_MOBILE',
-        drivingschool = true,
+        drivingschool = false,
         zoneOptions = { -- Used for when UseTarget is false
             length = 3.0,
             width = 3.0
